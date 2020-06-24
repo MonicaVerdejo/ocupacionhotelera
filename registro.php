9846cd967cadecd0c192a372bced4eca34915ab9
esdeath3x <66812479+esdeath3x@users.noreply.github.com>
2020-06-23 22:32:38 -0500

    correos validos commit verdejo

--- registro.php
+++ registro.php
@@ -2,33 +2,44 @@
 require 'db.php';
 
 if (!empty($_POST['email']) && !empty($_POST['contra'])) {
-  $db = new DB();
-  $stmtCorreosRegistrados = $db->connect()->prepare('SELECT correo FROM usuario WHERE correo = :email');
-  $stmtCorreosRegistrados->bindParam(':email', $_POST['email']);
-  $stmtCorreosRegistrados->execute();
-
-  if ($stmtCorreosRegistrados->rowCount() > 0) {
-    echo '<script type="text/javascript">
     
-    window.location.href="registrar.php";
-    alert("El correo que has proporcionado ya se encuentra registrado en otra cuenta, por favor intenta con otro");
-    </script>';
-  } else {
-    # if email doesnt exist 
-    $sql = "INSERT INTO usuario(usuario,correo, password, imagen_profile, rol_id) values (:usuario, :email, :contra,'defecto.png','2')";
-   
-    $stmt = $db->connect()->prepare($sql);
-    $stmt->bindParam(":usuario", $_POST['usuario']);
-    $stmt->bindParam(":email", $_POST['email']);
-    $password = password_hash($_POST['contra'], PASSWORD_BCRYPT);
-    $stmt->bindParam(":contra", $password);
+  $email = $_POST['email'] ;
 
+if (strpos($email, 'outlook.com') || strpos($email, '@gmail.com')|| strpos($email, '@hotmail.com')|| strpos($email, '@yahoo.es') !== false) {
+  $db = new DB();
+$stmtCorreosRegistrados = $conn->prepare('SELECT correo FROM usuario WHERE correo = :email');
+$stmtCorreosRegistrados->bindParam(':email', $_POST['email']);
+$stmtCorreosRegistrados->execute();
 
-    if ($stmt->execute()) {
-      $message = "Usuario creado";
-      header("Location: hotel_principal.php");
-    } else {
-      $message = "Error al crear usuario";
-    }
-  }
-} //if email and password post doesnt exist
+if ($stmtCorreosRegistrados->rowCount() > 0) {
+   echo'<script type="text/javascript">
+  
+  window.location.href="registrar.php";
+  alert("El correo que has proporcionado ya se encuentra registrado con otra cuenta, por favor intenta con otro");
+  </script>';
+  
+}else{# if email doesnt exist 
+    $sql = "INSERT INTO usuario(usuario,correo, password, rol_id, imagen_profile) values (:usuario, :email, :contra, '2','defecto.png')";
+$stmt = $conn->prepare($sql);
+$stmt->bindParam(":usuario", $_POST['usuario']);
+$stmt->bindParam(":email", $_POST['email']);
+$password = password_hash($_POST['contra'], PASSWORD_BCRYPT);
+$stmt->bindParam(":contra", $password);
+
+if ($stmt->execute()) {
+  $message = "Usuario creado";
+    echo "<script>location.href='hotel_principal.php';</script>";
+}else{
+   $message = "Error al crear usuario";
+}
+
+}
+}else{
+  echo'<script type="text/javascript">
+  
+  window.location.href="registrar.php";
+  alert("No es un correo admitido, intenta con gmail.com, hotmail.com, outlook.com o yahoo.es");
+  </script>';
+}  
+}
+?>
